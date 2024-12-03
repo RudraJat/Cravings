d72dac66ccff7a236f88b78acb5d7fc5af440b9d
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-12-03 21:18:21 +0530

    up

--- App.jsx
+++ App.jsx
@@ -45,19 +45,109 @@ const Header =()=>{
 };
 
 const RestaurantCard = (props) =>{
+    const {resData}=props;
+
     return(
         <div className="restro-card" style={{ backgroundColor: "#f0f0f0"}}>
             <img 
             className="restro-logo"
             src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/h7oibtpm7kqks8cqz3gh" 
             alt="restro-logo" />
-            <h3>{props.restroName}</h3>
-            <h5>{props.cuisine}</h5>
-            <h5>{props.rating}</h5>
-            <h5>{props.delTime}</h5>
+            <h3>{resData.info.name}</h3>
+            <h5>{resData.info.cuisines}</h5>
+            <h5>{resData.info.avgRating}</h5>
+            <h5>{resData.info.costForTwo}</h5>
+            <h5>{resData.info.sla.deliveryTime}</h5>
         </div>
     );
 };
+
+const resObj={
+    "info": {
+      "id": "151656",
+      "name": "Dev International",
+      "cloudinaryImageId": "enj3srsnhbltbom2ovvh",
+      "locality": "khajri road",
+      "areaName": "Mohan Nagar",
+      "costForTwo": "₹100 for two",
+      "cuisines": [
+        "North Indian",
+        "Chinese",
+        "Fast Food",
+        "Beverages"
+      ],
+      "avgRating": 4.3,
+      "parentId": "71556",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "401",
+      "sla": {
+        "deliveryTime": 52,
+        "lastMileTravel": 13.2,
+        "serviceability": "SERVICEABLE",
+        "slaString": "50-55 mins",
+        "lastMileTravelString": "13.2 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-12-03 22:30:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "₹100 OFF",
+        "subHeader": "ABOVE ₹249",
+        "discountTag": "FLAT DEAL"
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-2a054cc2-fb63-407d-b52d-4de7215c07a0"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/chhindwara/dev-international-khajri-road-mohan-nagar-rest151656",
+      "type": "WEBLINK"
+    }
+  };
 
 const Body = () => {
     return(
@@ -66,18 +156,7 @@ const Body = () => {
                 Search
             </div>
             <div className="restro-container">
-                 <RestaurantCard 
-                 restroName="Meghana Foods"
-                 cuisine="Biryani, North Indian, Asian"
-                 rating="4.3"
-                 delTime="36 mins"
-                 />
-                 <RestaurantCard 
-                 restroName="KFC"
-                 cuisine="Burger, Fast Food"
-                 rating="4.4"
-                 delTime="25 mins"
-                 />
+                 <RestaurantCard resData={resObj} />
             </div>
         </div>
     );
