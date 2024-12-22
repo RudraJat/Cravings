f651e71c258816a025bcd55f8892900a3f95f36a
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-12-22 09:13:34 +0530

    up

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,21 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
+// import resList from "../utils/mockData";
 import { useState  } from "react"; 
 
 const Body = () => {
+
+  //Local State Variable - Super powerful variable
+  const [listOfRestaurants]=useState([{
+    "info": {
+      "id": "234875",
+      "name": "Adil Hotel",
+      "cloudinaryImageId": "gp1ityra6utvzqn6ghnv",
+      "costForTwo": "₹150 for two",
+      "cuisines": [
+        "North Indian",
+        "Biryani",
+        "Tandoor"]}
+  }])
   return (
     <div className="body">
       <div className="filter">
@@ -16,10 +29,10 @@ const Body = () => {
         </button>
       </div>
       <div className="restro-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
-      </div>
+      </div> 
     </div>
   );
 };
