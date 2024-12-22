740be098bff857faa1bf458909d073f12d0d38fe
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2024-12-22 09:44:54 +0530

    ujp

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,7 @@ import { useState  } from "react";
 const Body = () => {
 
   //Local State Variable - Super powerful variable
-  const [listOfRestaurants]=useState([{
+  const [listOfRestaurants, setListOfRestaurants]=useState([{
     "info": {
       "id": "234875",
       "name": "Adil Hotel",
@@ -14,15 +14,34 @@ const Body = () => {
       "cuisines": [
         "North Indian",
         "Biryani",
-        "Tandoor"]}
-  }])
+        "Tandoor"],
+        "avgRating": 3.4}
+    },{
+    "info": {
+      "id": "234876",
+      "name": "Jat Hotel",
+      "cloudinaryImageId": "gp1ityra6utvzqn6ghnv",
+      "costForTwo": "₹150 for two",
+      "cuisines": [
+        "North Indian",
+        "Biryani",
+        "Tandoor"],
+        "avgRating": 4.3}
+    }]);
+     
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("Button Clicked");
+            const filteredList=listOfRestaurants.filter(
+              (res)=>res.info.avgRating>4
+            );
+
+            setListOfRestaurants(filteredList);
+            
+            console.log(filteredList);
           }}
         >
           Top Rated Restaurants
