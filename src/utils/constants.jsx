d5910b12f8dd410631b5f161c23579d5de86acb0
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-15 17:44:24 +0530

    Remove all project files including package.json, components, utils, and styles to reset the project structure.

--- src/utils/constants.jsx
+++
@@ -1,6 +0,0 @@
-export const CDN_URL=
-    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LOGO_URL=
-    "https://thumbs.dreamstime.com/z/food-delivery-logo-template-vector-icon-illustration-170869600.jpg?ct=jpeghttps://img.freepik.com/free-vector/food-shopping-logo-template-design_460848-10299.jpg?t=st=1733669565~exp=1733673165~hmac=e3ba00400eac324d94b4ce8eeae5a082203a12d95607037a238db8ac0bd2bd8a&w=900";
-
