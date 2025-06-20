fcaf218d45a1f1bd09edc5a79c73066011f58705
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-20 18:39:20 +0530

    Refactor code structure for improved readability and maintainability

--- App.jsx
+++ App.jsx
@@ -1,2 +1,17 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h2", { id: "heading", key: "1" }, "Hello World!"),
+    React.createElement("h3", { id: "sibling", key: "2" }, "this is sibling"),
+  ])
+);
+
+// const heading = React.createElement("h1", {id: "heading"}, "Welcome to React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
