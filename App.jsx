0d9e77af4f62250bcf4697a3d23faf9f021ef97f
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-21 18:47:43 +0530

    Add id attribute to heading in App component

--- App.jsx
+++ App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading=<h1>Namaste React</h1>;
+const jsxHeading=<h1 id="heading"> Namaste React </h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+ 
 root.render(jsxHeading);
