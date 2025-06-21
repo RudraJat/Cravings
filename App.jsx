6704703376461df644b80ca7e061054116fda9fd
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-21 18:29:44 +0530

    Add initial React setup with rendering of heading

--- App.jsx
+++ App.jsx
@@ -1,3 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const jsxHeading=<h1>Namaste React</h1>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
