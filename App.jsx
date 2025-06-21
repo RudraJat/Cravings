b8c34ec0a2c420a1d1656742bf00c30f78873651
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-21 18:14:10 +0530

    Update .gitignore and modify index.html content

--- App.jsx
+++ App.jsx
@@ -1,17 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h2", { id: "heading", key: "1" }, "Hello World!"),
-    React.createElement("h3", { id: "sibling", key: "2" }, "this is sibling"),
-  ])
-);
-
-// const heading = React.createElement("h1", {id: "heading"}, "Welcome to React");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
