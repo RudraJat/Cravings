626d96010039c8438e06911dd3641886ee638205
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-26 09:24:47 +0530

    Refactor Header component structure and add initial style.css for layout and logo styling

--- App.jsx
+++ App.jsx
@@ -4,19 +4,20 @@ import ReactDOM from "react-dom/client";
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo">
+      <div className="logo-container">
         <img
+          className="logo-img"
           src="https://file.aiquickdraw.com/imgcompressed/img/compressed_1de531cc9e3f6d6015240aafc950996c.webp"
           alt="logo-img"
         />
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
       </div>
     </div>
   );
