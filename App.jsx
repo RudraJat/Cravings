60c4350d8765617b2dc263534bdd22cb10b806ce
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-26 09:17:01 +0530

    Refactor App.jsx and index.html to implement Header component and update script references

--- App.jsx
+++ App.jsx
@@ -9,14 +9,24 @@ const Header = () => {
           src="https://file.aiquickdraw.com/imgcompressed/img/compressed_1de531cc9e3f6d6015240aafc950996c.webp"
           alt="logo-img"
         />
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 };
 const AppLayout = () => {
-  return <div className="app-layout"></div>;
+  return <div className="app-layout">
+    <Header />
+  </div>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<AppLayout/>);
