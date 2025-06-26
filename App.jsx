42770c9c7f34221e50e79b0a31335698c116866c
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-26 10:49:53 +0530

    Add Body component for search functionality and layout structure

--- App.jsx
+++ App.jsx
@@ -22,9 +22,34 @@ const Header = () => {
     </div>
   );
 };
+
+const RestaurantCard=()=>{
+    return(
+        <div className="res-card">
+            <img className="res-img" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/2469fa88ee9b0b5d1366ba88f2a7fa7f" alt="ich-coff-img" />
+            <h3>Indian Coffee House</h3>
+            <h4>Rating: 4.5 stars</h4>
+            <h4>Cost for Two: ₹300</h4>
+        </div>
+    )
+};
+
+const Body=()=>{
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {/* restaurant container */}
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+};
+
 const AppLayout = () => {
   return <div className="app-layout">
-    <Header />
+    <Header/>
+    <Body/>
   </div>;
 };
 
