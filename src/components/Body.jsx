79638dad704205fa06207a298bdee410b036fb13
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-06-30 11:50:36 +0530

    Implement filter functionality in Body component to display restaurants with ratings above 4.3

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,34 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-
+import { useState } from "react";
 
 function Body() {
+  const [listOfRestaurants, setlistOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
       {/* <div className="search">Search</div> */}
-
       <div className="filter-btn">
-        <button className="btn">Rating above 4⭐</button>
+        <button
+          className="btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.card.card.info.avgRating > 4.3
+            );
+            setlistOfRestaurants(filteredList);
+          }}
+        >
+          Rating above 4.3⭐
+        </button>
       </div>
       <div className="res-container">
         {/* restaurant container */}
-        {
-          resList.map((restaurant) => (
-            <RestaurantCard key={restaurant.card.card.info.id} resData={restaurant} />
-          ))
-        }
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant.card.card.info.id}
+            resData={restaurant}
+          />
+        ))}
       </div>
     </div>
   );
