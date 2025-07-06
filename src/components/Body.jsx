d6dad197f79a8cce6a45eb60e5b554e9da7dfb87
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-07-06 16:37:24 +0530

    Refactor fetchData function in Body component for improved readability and ensure restaurant list is initialized correctly

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,6 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 
-
-
-
 function Body() {
   const [listOfRestaurants, setlistOfRestaurants] = useState([]);
 
@@ -11,16 +8,18 @@ function Body() {
     fetchData();
   }, []);
 
-  const fetchData= async() =>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.2124007&lng=78.1772053&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.2124007&lng=78.1772053&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
     const json = await data.json();
     setlistOfRestaurants(
-  json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || []
-);
-  console.log(json);
-  }
- return (
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    console.log(json);
+  };
+  return (
     <div className="body">
       {/* <div className="search">Search</div> */}
       <div className="filter-btn">
@@ -38,13 +37,11 @@ function Body() {
       </div>
       <div className="res-container">
         {/* restaurant container */}
-        {listOfRestaurants.filter((res)=>res?.info).map((restaurant) => (
-  <RestaurantCard
-    key={restaurant?.info?.id}
-    resData={restaurant}
-  />
-))}
-
+        {listOfRestaurants
+          .filter((res) => res?.info)
+          .map((restaurant) => (
+            <RestaurantCard key={restaurant?.info?.id} resData={restaurant} />
+          ))}
       </div>
     </div>
   );
