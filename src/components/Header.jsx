9aed312f0151a3cda4ca08e84c1d0dd8c34f6f47
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-07-07 11:42:46 +0530

    Refactor Header component for improved code readability and consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,13 @@
-import {LOGO_URL} from "../utils/constants";
+import { LOGO_URL } from "../utils/constants";
+import {useState} from "react";
 
 const Header = () => {
+  const [loginBtn, setloginBtn] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo-img"
-          src={LOGO_URL}
-          alt="logo-img"
-        />
+        <img className="logo-img" src={LOGO_URL} alt="logo-img" />
       </div>
       <div className="nav-items">
         <div className="nav-list">
@@ -16,6 +15,16 @@ const Header = () => {
           <div>Contact Us</div>
           <div>About Us</div>
           <div>Cart</div>
+          <button
+            className="login-btn"
+            onClick={() => {
+              loginBtn === "Login"
+                ? setloginBtn("Logout")
+                : setloginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
         </div>
       </div>
     </div>
