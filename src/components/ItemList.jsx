1ce4b3258c502885ca74179a276e3c3606aa0432
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-07-14 20:03:47 +0530

    added button to menu for add to cart

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -28,15 +28,20 @@ const ItemList = ({ items }) => {
                 ₹{displayPrice || "N/A"}
               </p>
             </div>
-
+            <div>
+            <div className="relative left-10 top-28">
+            <button className="p-1 px-6 bg-white rounded hover:bg-gray-100 font-bold  text-green-500 hover:scale-105 shadow-lg m-auto">ADD</button>
+            </div>
+              <div>
             {imageId && (
               <img
-  src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_200/${imageId}`}
-  alt={name}
-  className="rounded-2xl object-cover w-32 h-24 ml-4 border border-gray-200 shadow-md hover:shadow-green-300 hover:scale-105 transition duration-300 ease-in-out"
-/>
-
+                src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_200/${imageId}`}
+                alt={name}
+                className="rounded-2xl object-cover w-32 h-24 ml-4 border border-gray-200 shadow-md hover:shadow-green-300  transition duration-300 ease-in-out"
+              />
             )}
+            </div>
+            </div>
           </div>
         );
       })}
