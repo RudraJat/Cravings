c47591958c1994b78132e4b370433f43bccaf526
Rudrapratap Singh Jat <rps9827256181@gmail.com>
2025-07-27 17:48:23 +0530

    install redux and rtk in application

--- src/utils/userContext.jsx
+++ src/utils/userContext.jsx
@@ -3,7 +3,7 @@ import { createContext } from "react";
 // Creating React Context- It is used to avoid prop drilling
 // It allows us to share values between components without passing props manually at every level
 const userContext= createContext({
-    loggedInUser:"Rudu",
+    loggedInUser:"Rudu ",
 });
 
 export default userContext;
